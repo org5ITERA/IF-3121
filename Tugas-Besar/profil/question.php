509166d735104919e4b46a9d4046f5cf404ef57c
Unknown <sket_acmilan76@yahoo.co.id>
2017-10-18 20:51:40 +0700

    V3.1

--- Tugas-Besar/profil/question.php
+++ Tugas-Besar/profil/question.php
@@ -7,10 +7,6 @@ $uid = $info['id'];
 $tarik = "select * from question where id_user='$uid'";
 $run_tarik = mysqli_query($db,$tarik);
 
-$countanswer = "select * from answer where id_user='$uid' ";
-$run_count = mysqli_query($db,$countanswer);
-$count = mysqli_num_rows($run_count);
-
 while ($row_tarik=mysqli_fetch_array($run_tarik)){
 
   $id = $row_tarik['id'];
@@ -22,6 +18,11 @@ while ($row_tarik=mysqli_fetch_array($run_tarik)){
  ?>
 <div class="pertanyaan_recent">
     <fieldset>
+    <?php
+    $countanswer = "select * from answer where id_question='$id' ";
+    $run_count = mysqli_query($db,$countanswer);
+    $count = mysqli_num_rows($run_count);
+    ?>
       <h2><a href="../pertanyaan-populer.php?id=<?php echo "$id" ?>"><?php echo"$judul" ?></a></h2>
       <br>
       <h4><?php echo" $count Komentar | $date"?></h4>
